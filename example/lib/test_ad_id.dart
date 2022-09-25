5e2d782065a7a916890ad622881bac6d956237ce
ssk <developer.kss@gmail.com>
2022-09-26 07:16:12 +0900

    test ad ids

---
+++ example/lib/test_ad_id.dart
@@ -0,0 +1,10 @@
+class TestAdId {
+  final String appOpening = 'ca-app-pub-3940256099942544/3419835294';
+  final String banner = 'ca-app-pub-3940256099942544/6300978111';
+  final String interstitial = 'ca-app-pub-3940256099942544/1033173712';
+  final String interstitialVideo = 'ca-app-pub-3940256099942544/8691691433';
+  final String rewarded = 'ca-app-pub-3940256099942544/5224354917';
+  final String rewardedInterstitial = 'ca-app-pub-3940256099942544/5354046379';
+  final String nativeAdvanced = 'ca-app-pub-3940256099942544/2247696110';
+  final String nativeAdvancedVideo = 'ca-app-pub-3940256099942544/1044960115';
+}
