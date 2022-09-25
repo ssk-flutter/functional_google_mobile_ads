ec5fb5080ac1fb2fc5c866baba9fcf42887f6105
ssk <developer.kss@gmail.com>
2022-09-26 08:12:59 +0900

    ios test id

--- example/lib/test_ad_id.dart
+++ example/lib/test_ad_id.dart
@@ -1,10 +1,36 @@
+import 'package:selector/selector.dart';
+
 class TestAdId {
-  final String appOpening = 'ca-app-pub-3940256099942544/3419835294';
-  final String banner = 'ca-app-pub-3940256099942544/6300978111';
-  final String interstitial = 'ca-app-pub-3940256099942544/1033173712';
-  final String interstitialVideo = 'ca-app-pub-3940256099942544/8691691433';
-  final String rewarded = 'ca-app-pub-3940256099942544/5224354917';
-  final String rewardedInterstitial = 'ca-app-pub-3940256099942544/5354046379';
-  final String nativeAdvanced = 'ca-app-pub-3940256099942544/2247696110';
-  final String nativeAdvancedVideo = 'ca-app-pub-3940256099942544/1044960115';
+  final String appOpening = androidOrElse(
+    'ca-app-pub-3940256099942544/3419835294',
+    'ca-app-pub-3940256099942544/5662855259',
+  );
+  final String banner = androidOrElse(
+    'ca-app-pub-3940256099942544/6300978111',
+    'ca-app-pub-3940256099942544/2934735716',
+  );
+  final String interstitial = androidOrElse(
+    'ca-app-pub-3940256099942544/1033173712',
+    'ca-app-pub-3940256099942544/4411468910',
+  );
+  final String interstitialVideo = androidOrElse(
+    'ca-app-pub-3940256099942544/8691691433',
+    'ca-app-pub-3940256099942544/5135589807',
+  );
+  final String rewarded = androidOrElse(
+    'ca-app-pub-3940256099942544/5224354917',
+    'ca-app-pub-3940256099942544/1712485313',
+  );
+  final String rewardedInterstitial = androidOrElse(
+    'ca-app-pub-3940256099942544/5354046379',
+    'ca-app-pub-3940256099942544/6978759866',
+  );
+  final String nativeAdvanced = androidOrElse(
+    'ca-app-pub-3940256099942544/2247696110',
+    'ca-app-pub-3940256099942544/3986624511',
+  );
+  final String nativeAdvancedVideo = androidOrElse(
+    'ca-app-pub-3940256099942544/1044960115',
+    'ca-app-pub-3940256099942544/2521693316',
+  );
 }
