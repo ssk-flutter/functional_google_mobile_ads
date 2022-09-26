ded50f5d91c87b31ca219c3ef8fe20c27c9c0b71
ssk <developer.kss@gmail.com>
2022-09-27 05:30:48 +0900

    library 코드 위치 이동

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,10 +3,8 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:functional_google_mobile_ads/functional_google_mobile_ads.dart';
-import 'package:functional_google_mobile_ads_example/test_ad_id.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-
-import 'functional_admob_banner.dart';
+import 'package:selector/selector.dart';
+import 'package:functional_google_mobile_ads/functional_admob_flutter.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -73,4 +71,39 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+}
+
+class TestAdId {
+  static final String appOpening = androidOrElse(
+    'ca-app-pub-3940256099942544/3419835294',
+    'ca-app-pub-3940256099942544/5662855259',
+  );
+  static final String banner = androidOrElse(
+    'ca-app-pub-3940256099942544/6300978111',
+    'ca-app-pub-3940256099942544/2934735716',
+  );
+  static final String interstitial = androidOrElse(
+    'ca-app-pub-3940256099942544/1033173712',
+    'ca-app-pub-3940256099942544/4411468910',
+  );
+  static final String interstitialVideo = androidOrElse(
+    'ca-app-pub-3940256099942544/8691691433',
+    'ca-app-pub-3940256099942544/5135589807',
+  );
+  static final String rewarded = androidOrElse(
+    'ca-app-pub-3940256099942544/5224354917',
+    'ca-app-pub-3940256099942544/1712485313',
+  );
+  static final String rewardedInterstitial = androidOrElse(
+    'ca-app-pub-3940256099942544/5354046379',
+    'ca-app-pub-3940256099942544/6978759866',
+  );
+  static final String nativeAdvanced = androidOrElse(
+    'ca-app-pub-3940256099942544/2247696110',
+    'ca-app-pub-3940256099942544/3986624511',
+  );
+  static final String nativeAdvancedVideo = androidOrElse(
+    'ca-app-pub-3940256099942544/1044960115',
+    'ca-app-pub-3940256099942544/2521693316',
+  );
 }
---
+++ lib/functional_admob_flutter.dart
@@ -0,0 +1,2 @@
+export 'package:google_mobile_ads/google_mobile_ads.dart';
+export 'package:functional_google_mobile_ads/functional_admob_banner.dart';
